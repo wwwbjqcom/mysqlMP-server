9586285c9809851bd466d6bf2aa240d61bf7d668
大神 <manito@potato.im>
2020-01-14 17:57:26 +0800

    1

--- src/webroute/new_route.rs
+++ src/webroute/new_route.rs
@@ -327,13 +327,14 @@ pub fn alter_interface(data: web::Data<DbInfo>, info: web::Json<PostAlter>) -> H
     if let Err(e) = data.check_user_info(&info.hook_id){
         return ResponseState::error(e.to_string());
     }
-
+    info!("bb");
     match data.get_all_node_state() {
         Ok(response_alter) => {
             info!("{:?}", &response_alter);
             return response_value(&response_alter);
         }
         Err(e) => {
+            info!("aaa");
             return ResponseState::error(e.to_string());
         }
     }
